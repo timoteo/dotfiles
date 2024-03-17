76366954024a1c420b4f759de3e7ff805aad34ca
Timo <timo@timothyohara.com>
2024-03-17 12:14:09 -1000

    tweak fonts

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -4,6 +4,10 @@ return {
 	color_scheme = 'Catppuccin Mocha',
 	enable_tab_bar = false,
 	font_size = 16.0,
+	font = wezterm.font_with_fallback {
+		'Cascadia Code',
+		'JetBrains Mono',
+	},
 	-- macos_window_background_blur = 40,
 	macos_window_background_blur = 30,
 	
