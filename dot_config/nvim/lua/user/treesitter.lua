923814404fae3013e6e0ff3bd27f87db6154d447
Timo <timo@timothyohara.com>
2024-07-29 07:31:33 -1000

    Update formatting

--- dot_config/nvim/lua/user/treesitter.lua
+++ dot_config/nvim/lua/user/treesitter.lua
@@ -1,111 +1,111 @@
 local M = {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "apple/pkl-neovim",
-    "nvim-treesitter/nvim-treesitter-textobjects",
-    "windwp/nvim-ts-autotag",
-  },
-  event = { "BufReadPost", "BufNewFile" },
-  build = ":TSUpdate",
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"apple/pkl-neovim",
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		"windwp/nvim-ts-autotag",
+	},
+	event = { "BufReadPost", "BufNewFile" },
+	build = ":TSUpdate",
 }
 
 function M.config()
-  local treesitter = require "nvim-treesitter.configs"
-  treesitter.setup {
-    -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = {
-      "astro",
-      "bash",
-      "css",
-      "dockerfile",
-      "gitignore",
-      "go",
-      "html",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "markdown_inline",
-      "pkl",
-      "python",
-      "regex",
-      "rust",
-      "sql",
-      "tsx",
-      "typescript",
-      "vimdoc",
-      "yaml",
-    },
-    autotag = { enable = true },
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<c-space>",
-        node_incremental = "<c-space>",
-        -- scope_incremental = "<c-s>",
-        scope_incremental = false,
-        -- node_decremental = "<c-backspace>",
-        node_decremental = "<bs>",
-      },
-    },
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["aa"] = "@parameter.outer",
-          ["ia"] = "@parameter.inner",
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
-          ["ii"] = "@conditional.inner",
-          ["ai"] = "@conditional.outer",
-          ["il"] = "@loop.inner",
-          ["al"] = "@loop.outer",
-          ["at"] = "@comment.outer",
-        },
-      },
-      move = {
-        enable = true,
-        set_jumps = true, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          ["]m"] = "@function.outer",
-          ["]]"] = "@class.outer",
-        },
-        goto_next_end = {
-          ["]M"] = "@function.outer",
-          ["]["] = "@class.outer",
-        },
-        goto_previous_start = {
-          ["[m"] = "@function.outer",
-          ["[["] = "@class.outer",
-        },
-        goto_previous_end = {
-          ["[M"] = "@function.outer",
-          ["[]"] = "@class.outer",
-        },
-        -- goto_next = {
-        --   [']i'] = "@conditional.inner",
-        -- },
-        -- goto_previous = {
-        --   ['[i'] = "@conditional.inner",
-        -- }
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>a"] = "@parameter.inner",
-        },
-        swap_previous = {
-          ["<leader>A"] = "@parameter.inner",
-        },
-      },
-    },
-  }
+	local treesitter = require("nvim-treesitter.configs")
+	treesitter.setup({
+		-- Add languages to be installed here that you want installed for treesitter
+		ensure_installed = {
+			"astro",
+			"bash",
+			"css",
+			"dockerfile",
+			"gitignore",
+			"go",
+			"html",
+			"javascript",
+			"json",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"pkl",
+			"python",
+			"regex",
+			"rust",
+			"sql",
+			"tsx",
+			"typescript",
+			"vimdoc",
+			"yaml",
+		},
+		autotag = { enable = true },
+		highlight = { enable = true },
+		indent = { enable = true },
+		incremental_selection = {
+			enable = true,
+			keymaps = {
+				init_selection = "<c-space>",
+				node_incremental = "<c-space>",
+				-- scope_incremental = "<c-s>",
+				scope_incremental = false,
+				-- node_decremental = "<c-backspace>",
+				node_decremental = "<bs>",
+			},
+		},
+		textobjects = {
+			select = {
+				enable = true,
+				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+				keymaps = {
+					-- You can use the capture groups defined in textobjects.scm
+					["aa"] = "@parameter.outer",
+					["ia"] = "@parameter.inner",
+					["af"] = "@function.outer",
+					["if"] = "@function.inner",
+					["ac"] = "@class.outer",
+					["ic"] = "@class.inner",
+					["ii"] = "@conditional.inner",
+					["ai"] = "@conditional.outer",
+					["il"] = "@loop.inner",
+					["al"] = "@loop.outer",
+					["at"] = "@comment.outer",
+				},
+			},
+			move = {
+				enable = true,
+				set_jumps = true, -- whether to set jumps in the jumplist
+				goto_next_start = {
+					["]m"] = "@function.outer",
+					["]]"] = "@class.outer",
+				},
+				goto_next_end = {
+					["]M"] = "@function.outer",
+					["]["] = "@class.outer",
+				},
+				goto_previous_start = {
+					["[m"] = "@function.outer",
+					["[["] = "@class.outer",
+				},
+				goto_previous_end = {
+					["[M"] = "@function.outer",
+					["[]"] = "@class.outer",
+				},
+				-- goto_next = {
+				--   [']i'] = "@conditional.inner",
+				-- },
+				-- goto_previous = {
+				--   ['[i'] = "@conditional.inner",
+				-- }
+			},
+			swap = {
+				enable = true,
+				swap_next = {
+					["<leader>a"] = "@parameter.inner",
+				},
+				swap_previous = {
+					["<leader>A"] = "@parameter.inner",
+				},
+			},
+		},
+	})
 end
 
 return M
